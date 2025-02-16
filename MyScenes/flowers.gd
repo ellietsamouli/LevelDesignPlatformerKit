1b2558fda65f6b6d68b1e8bc08a69ca2324e7b38
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2025-02-16 18:28:48 +0100

    Finished the aloe vera, added collision to the pot

--- MyScenes/flowers.gd
+++ MyScenes/flowers.gd
@@ -1,6 +1,8 @@
-extends Node3D  # This ensures the script runs on a 3D object
+extends Node3D  
 
 @export var rotation_speed: float = 60.0  # Speed of rotation in degrees per second
+@export var can_spin: bool = true  # Checkbox to enable/disable spinning
 
 func _process(delta):
-	rotation_degrees.y += rotation_speed * delta  # Rotates around Y-axis
+	if can_spin:  # Only spin if the checkbox is checked
+		rotation_degrees.y += rotation_speed * delta  # Rotates around Y-axis
